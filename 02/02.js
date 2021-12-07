110422c8af201e0dfed9ab8ecc91aead6336c1c2
Patrick Kellar <pjkellar@gmail.com>
2021-12-07 18:10:13 -0500

    Day 2 complete... I'm only 5 days late :P

--- 02/02.js
+++ 02/02.js
@@ -1,7 +1,7 @@
 fs = require('fs');
 
 // Common starting point, get data from file...
-const input = fs.readFileSync(`./sample.txt`, 'utf8');
+const input = fs.readFileSync(`./input.txt`, 'utf8');
 
 // Solution starts here
 
@@ -14,17 +14,13 @@ let horizontal = 0;
 let depth = 0;
 
 data.forEach((line) => {
-  let commands = line.split(' ');
-  switch (commands[0]) {
-    case 'forward':
-      horizontal = horizontal + parseInt(commands[1]);
-      break;
-    case 'down':
-      depth = depth + parseInt(commands[1]);
-      break;
-    case 'up':
-      depth = depth - parseInt(commands[1]);
-      break;
+  let [ command, value ]  = line.split(' ');
+  if(command === 'forward'){
+    horizontal = horizontal + parseInt(value);
+  } else if(command === 'down'){
+    depth = depth + parseInt(value);
+  } else if(command === 'up'){
+    depth = depth - parseInt(value);
   }
 });
 
@@ -38,22 +34,35 @@ let depth2 = 0;
 let aim = 0;
 
 data.forEach((line) => {
-  let commands = line.split(' ');
-  switch (commands[0]) {
-    case 'forward':
-      horizontal2 = horizontal2 + parseInt(commands[1]);
-      depth2 = aim * parseInt(commands[1]);
-      break;
-    case 'down':
-      aim = aim + parseInt(commands[1]);
-      break;
-    case 'up':
-      aim = aim - parseInt(commands[1]);
-      break;
+  let [ command, value ]  = line.split(' ');
+  if(command === 'forward'){
+    horizontal2 = horizontal2 + parseInt(value);
+    // Need adjust aim
+    depth2 = aim * parseInt(value) + depth2
+  } else if(command === 'down'){
+    aim = aim + parseInt(value);
+  } else if(command === 'up'){
+    aim = aim - parseInt(value);
   }
-  console.log(`horizontal:${horizontal2}`);
-  console.log(`depth:${depth2}`);
-  console.log(`aim:${aim}`);
+
+
+  // let commands = line.split(' ');
+  // switch (commands[0]) {
+  //   case 'forward':
+  //     horizontal2 = horizontal2 + parseInt(commands[1]);
+  //     depth2 = aim * parseInt(commands[1]);
+  //     break;
+  //   case 'down':
+  //     aim = aim + parseInt(commands[1]);
+  //     break;
+  //   case 'up':
+  //     aim = aim - parseInt(commands[1]);
+  //     break;
+  // }
+  // console.log(`horizontal:${horizontal2}`);
+  // console.log(`depth:${depth2}`);
+  // console.log(`aim:${aim}`);
+  // console.log('---');
 });
 
 partB = horizontal2 * depth2;
